59e1f6234ab1fdeda2d6321b470af2c4084065b8
Valeriy Osipov <info@vosipov.com>
2017-07-22 17:22:57 +0300

    Add talks app

--- src/survivalguide/settings.py
+++ src/survivalguide/settings.py
@@ -31,15 +31,21 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+)
+THIRD_PARTY_APPS = (
     'crispy_forms',
-]
+)
+LOCAL_APPS = (
+    'talks',
+)
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ src/talks/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import TalkList
+
+admin.site.register(TalkList)
