431f1dd5b13271947972d844e625d15fb896570f
Valeriy Osipov <info@vosipov.com>
2017-07-23 14:02:09 +0300

    Add RestrictToUserMixin

--- src/talks/views.py
+++ src/talks/views.py
@@ -4,18 +4,17 @@ from braces import views
 from . import models
 
 
-class TalkListListView(views.LoginRequiredMixin, generic.ListView):
-    model = models.TalkList
-
+class RestrictToUserMixin(object):
     def get_queryset(self):
-        return self.request.user.lists.all()
-
-
-class TalkListDetailView(views.LoginRequiredMixin, views.PrefetchRelatedMixin, generic.DetailView):
-    model = models.TalkList
-    prefetch_related = ('talks', )
-
-    def get_queryset(self):
-        queryset = super(TalkListDetailView, self).get_queryset()
+        queryset = super(RestrictToUserMixin, self).get_queryset()
         queryset = queryset.filter(user=self.request.user)
         return queryset
+
+
+class TalkListListView(views.LoginRequiredMixin, RestrictToUserMixin, generic.ListView):
+    model = models.TalkList
+
+
+class TalkListDetailView(views.LoginRequiredMixin, views.PrefetchRelatedMixin, RestrictToUserMixin, generic.DetailView):
+    model = models.TalkList
+    prefetch_related = ('talks', )
