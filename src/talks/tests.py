2b36f86891446a36cea26a2dca18026340ef55e5
Valeriy Osipov <info@vosipov.com>
2017-08-17 23:03:07 +0300

    Add unit tests

--- src/talks/tests.py
+++ src/talks/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from talks.models import TalkList, Talk
 
-# Create your tests here.
+
+class TalkListTests(TestCase):
+    def test_str(self):
+        t = TalkList(name='MyName')
+        self.assertEqual(str(t), 'MyName')
+
+
+class TalkTests(TestCase):
+    def test_str(self):
+        t = Talk(name='MyName')
+        self.assertEqual(str(t), 'MyName')
