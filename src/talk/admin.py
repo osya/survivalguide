b23e8aa7f1b49a042c599d5d09927f4c1f18926d
Valeriy Osipov <info@vosipov.com>
2018-02-28 13:04:20 +0300

    Fix creating new Talk via Admin panel

--- src/talk/admin.py
+++ src/talk/admin.py
@@ -2,9 +2,4 @@ from django.contrib import admin
 
 from talk.models import Talk
 
-
-class TalkAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name', )}
-
-
-admin.site.register(Talk, TalkAdmin)
+admin.site.register(Talk)
