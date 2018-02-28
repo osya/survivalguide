3b9bebbf8cb8ec780e440f0dd9d58c4d8c28c1bb
Valeriy Osipov <info@vosipov.com>
2018-02-28 13:13:04 +0300

    Fix slug in Admin panel

--- src/talklist/admin.py
+++ src/talklist/admin.py
@@ -3,9 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from talklist.models import TalkList
 
-
-class TalkListAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
-
-
-admin.site.register(TalkList, TalkListAdmin)
+admin.site.register(TalkList)
